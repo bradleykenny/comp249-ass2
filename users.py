6ffc888d5ba1141ceba75663fda757e21310e58a
bradleykenny <bkenny283@gmail.com>
2018-04-24 10:16:23 +1000

    Completed user session functions.

--- users.py
+++ users.py
@@ -2,9 +2,13 @@
 Created on Mar 26, 2012
 
 @author: steve
+@student: bradley kenny [SID: 45209723]
 """
 
 from database import *
+import uuid
+import requests
+from bottle import response, request
 
 # this variable MUST be used as the name for the cookie used by this application
 COOKIE_NAME = 'sessionid'
@@ -21,18 +25,43 @@ def check_login(db, usernick, password):
 			return False
 
 def generate_session(db, usernick):
-    """create a new session and add a cookie to the response object (bottle.response)
-    user must be a valid user in the database, if not, return None
-    There should only be one session per user at any time, if there
-    is already a session active, use the existing sessionid in the cookie
-    """
+	cur = db.cursor()
+	cur.execute("SELECT * FROM users WHERE nick=?", (usernick,))
+	user_row = cur.fetchone()
+	if (user_row != None):
+		cur.execute("SELECT * FROM sessions WHERE usernick=?", (usernick,))
+		ses_row = cur.fetchone()
+
+		if (ses_row != None):
+			if (ses_row[1] == user_row[0]):
+				session_id = ses_row[0]
+		else:
+			session_id = str(uuid.uuid4())
+			cur.execute("INSERT INTO sessions VALUES (?,?)", (session_id, usernick))
+			db.commit()
+
+		response.set_cookie(COOKIE_NAME, session_id)
+		return session_id
+	else:
+		return None
 
 
 def delete_session(db, usernick):
-    """remove all session table entries for this user"""
+    # remove all session table entries for this user
+	cur = db.cursor()
+	cur.execute("SELECT * FROM sessions WHERE usernick=?", (usernick,))
+	user_row = cur.fetchone()
+	if (user_row != None):
+		cur.execute("DELETE FROM sessions WHERE usernick=?", (usernick,))
 
 
 def session_user(db):
-    """try to
-    retrieve the user from the sessions table
-    return usernick or None if no valid session is present"""
+    # try to retrieve the user from the sessions table
+	# return usernick or None if no valid session is present
+	session_id = request.get_cookie(COOKIE_NAME)
+	cur = db.cursor()
+	cur.execute("SELECT * FROM sessions WHERE sessionid=?", (session_id,))
+	row = cur.fetchone()
+	if not row:
+		return None
+	return row[1]
