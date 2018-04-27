9a17cdcbb765196229699d66154cca1ecb3126db
bradleykenny <bkenny283@gmail.com>
2018-04-27 15:39:01 +1000

    Failed login completely works. CSS correct for '/wrong_password' too.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 __author__ = 'Bradley Kenny'
 
-from bottle import Bottle, template, static_file, request, response, redirect
+from bottle import Bottle, template, static_file, request, response, redirect, HTTPResponse
 from interface import *
 from users import *
 
@@ -17,8 +17,8 @@ def index(db):
         'title': 'Welcome to Jobs',
         'message': 'Welcome to Jobs',
         'positions': position_list(db),
-		'log_in':  loginform_ornot(db),
-		'job_post': newjobform_ornot(db),
+        'log_in':  loginform_ornot(db),
+        'job_post': newjobform_ornot(db),
     }
     return template('index', info)
 
@@ -28,114 +28,113 @@ def about(db):
     info = {
         'title': 'Jobs | About',
         'message': 'About Us',
-		'log_in': loginform_ornot(db),
+        'log_in': loginform_ornot(db),
     }
     return template('about.html', info)
 
 
 @app.route('/positions/<id>')
 def positions(db, id):
-	info = {
+    info = {
         'title': 'Jobs | %s' % position_get(db, int(id))[3],
         'message': 'Your next job?',
         'positions': position_get(db, int(id)),
-		'log_in': loginform_ornot(db),
+        'log_in': loginform_ornot(db),
     }
-	return template('position.html', info)
+    return template('position.html', info)
 
 
 @app.route('/login', method="POST")
 def login(db):
-	nick = request.forms.get('nick')
-	password = request.forms.get('password')
-	if check_login(db, nick, password):
-		generate_session(db, nick)
-		return redirect('/')
-	elif correct_username(db, nick):
-		response.
-		return redirect('/wrong_password', 200)
-	else:
-		return redirect('/')
+    nick = request.forms.get('nick')
+    password = request.forms.get('password')
+    if check_login(db, nick, password):
+        generate_session(db, nick)
+        return redirect('/')
+    elif correct_username(db, nick):
+        response.status = 200
+        return wrong_pw(db)
+    else:
+        return redirect('/')
 
 
 @app.route('/logout')
 def logout(db):
-	nick = session_user(db)
-	delete_session(db, nick)
-	return redirect('/')
+    nick = session_user(db)
+    delete_session(db, nick)
+    return redirect('/')
 
 
 @app.route('/post', method="POST")
 def post(db):
-	user = session_user(db)
-	title = request.forms.get('title')
-	location = request.forms.get('location')
-	company = request.forms.get('company')
-	description = request.forms.get('description')
-	if user != None:
-		position_add(db, user, title, location, company, description)
-		return redirect('/')
-	else:
-		return redirect('/')
-
-
-@app.route('/wrong_password')
-def wrong_pw(db):
-    info = {
-        'title': 'Jobs | Wrong Password',
-        'message': 'Wrong Password',
-		'log_in': "",
-    }
-    return template('wrong_pw.html', info)
+    user = session_user(db)
+    title = request.forms.get('title')
+    location = request.forms.get('location')
+    company = request.forms.get('company')
+    description = request.forms.get('description')
+    if user != None:
+        position_add(db, user, title, location, company, description)
+        return redirect('/')
+    else:
+        return redirect('/')
 
 
 # ===== HELPER METHODS =====
 
 def loginform_ornot(db):
-	name = session_user(db)
-	if name != None:
-		return ("""
-			<form action="/logout" id="logoutform">
-				<p>Logged in as %s</p>
-				<input type='submit' value="Logout"/>
-			</form>
-			""" % name)
-	else:
-		return("""
-			<form action="/login" id="loginform" method="POST">
-				<input type="text" name="nick" placeholder="Username"/>
-				<input type="password" name="password" placeholder="Password"/>
-				<input type='submit' value="Login"/>
-			</form>
-			""")
+    name = session_user(db)
+    if name != None:
+        return ("""
+            <form action="/logout" id="logoutform">
+            <p>Logged in as %s</p>
+            <input type='submit' value="Logout"/>
+            </form>
+            """ % name)
+    else:
+        return("""
+            <form action="/login" id="loginform" method="POST">
+            <input type="text" name="nick" placeholder="Username"/>
+            <input type="password" name="password" placeholder="Password"/>
+            <input type='submit' value="Login"/>
+            </form>
+            """)
 
 
 def newjobform_ornot(db):
-	name = session_user(db)
-	if name != None:
-		return ("""
-			<h1 style="font-size: 20px; font-family: 'Merriweather';">POST A NEW JOB</h1>
-			<div id="postform_card" class="card">
-				<form action="/post" id="postform" method="POST">
-					<input type="text" name="title" placeholder="Title" class="post_title"/>
-					<input type="text" name="location" placeholder="Location" class="post_location"/>
-					<input type="text" name="company" placeholder="Company" class="post_company"/>
-					<textarea name="description" placeholder="Description" class="post_description"></textarea>
-					<input type='submit' value="Post" class="button"/>
-				</form>
-			</div>
-			""")
-	else:
-		return("")
+    name = session_user(db)
+    if name != None:
+        return ("""
+            <h1 style="font-size: 20px; font-family: 'Merriweather';">POST A NEW JOB</h1>
+            <div id="postform_card" class="card">
+                <form action="/post" id="postform" method="POST">
+                    <input type="text" name="title" placeholder="Title" class="post_title"/>
+                    <input type="text" name="location" placeholder="Location" class="post_location"/>
+                    <input type="text" name="company" placeholder="Company" class="post_company"/>
+                    <textarea name="description" placeholder="Description" class="post_description"></textarea>
+                    <input type='submit' value="Post" class="button"/>
+                </form>
+            </div>
+            """)
+    else:
+        return("")
 
 def correct_username(db, usernick):
-	cur = db.cursor()
-	cur.execute("SELECT * FROM users WHERE nick=?", (usernick,))
-	row = cur.fetchone()
-	if row != None:
-		return True
-	else:
-		return False
+    cur = db.cursor()
+    cur.execute("SELECT * FROM users WHERE nick=?", (usernick,))
+    row = cur.fetchone()
+    if row != None:
+        return True
+    else:
+        return False
+
+
+def wrong_pw(db):
+    info = {
+        'title': 'Jobs | Wrong Password',
+        'message': 'Wrong Password',
+        'log_in': "",
+    }
+    return template('wrong_pw.html', info)
 
 
 if __name__ == '__main__':
